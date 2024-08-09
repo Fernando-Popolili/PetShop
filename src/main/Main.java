853e1885a9763322ebd26a0f605186b5fc6ff718
Fernando-Popolili <fpopolili@gmail.com>
2024-08-09 14:13:54 -0300

    Criação da tela de login e da splash Screen

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+/*
+PETSHOP
+ 
+splash screen 
+login e senha
+*/
+
+package main;
+
+import view.SplashScreen_GUI;
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+        new SplashScreen_GUI().setVisible(true);
+    }
+    
+}
